3144f5a7e143f796836648f0943410330c79ddc2
João Victor Santos Lima <128315208+Jvsl34@users.noreply.github.com>
2023-11-01 11:52:28 -0300

    Add files via upload

---
+++ shmup2D/scripts/background.gd
@@ -0,0 +1,17 @@
+extends Sprite
+
+export(NodePath) var nave
+
+var time = 0.0
+
+func _ready():
+	if nave:
+		nave = get_node(nave)
+	else:
+		set_process(false)
+
+func _process(delta):
+	time += delta
+	material.set_shader_param("des" , (nave.global_position.x - 80) * 0.002 )
+	material.set_shader_param("time" , time)
+
